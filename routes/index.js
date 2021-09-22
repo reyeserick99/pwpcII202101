679fcb14f5d63020191a1dbfbece7dc3428e74b9
ERICKREYESG <rickreyes340@gmail.com>
2021-09-22 10:07:12 -0500

    master

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author: 'Erick Reyes G ', appName: ' WebApp'});
+});
+
+
+/*[BROWSER] --> request{info} -->  http --> [SERVER] --> GET --> [] -->/ [greeting] res --> [] --> [¨BROWSER] */
+
+/*AGREGANDO RUTA NUEVA*/
+router.get('/greeting', function(req, res, next){
+  res.send('BUENAS')
+
+});
+
+module.exports = router;
