f63562d1501155a1da81da629c237d81ee53ac42
reyeserick99 <rickreyes340@gmail.com>
2021-10-06 07:23:42 -0500

    Babel

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from '@s-routes/index';
+import usersRouter from '@s-routes/users';
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.use(logger('dev'));  /*Realiza la peticion */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); /*Parte y/o archivos estaticos ayuda a que pueda mandar la peticion mucho antes de arvhivos y/o imagenes*/ 
+app.use(express.static(path.join(__dirname,'..', 'public'))); /*Parte y/o archivos estaticos ayuda a que pueda mandar la peticion mucho antes de arvhivos y/o imagenes*/ 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: 'Erick Reyes G ', appName: ' WebApp', company:"awesome software"});
+  res.render('index', { title: 'Express', author: 'Reyes ', appName: ' WebApp', company:"awesome software"});
 });
 
 
