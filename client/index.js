b4faac525448753a75e4d3fc51c1c17d0a379f1f
ERICKREYESG <rickreyes340@gmail.com>
2021-11-02 22:59:43 -0600

    •	SESION 37 BARRA DE NAVEGACIÓN

--- client/index.js
+++ client/index.js
@@ -2,3 +2,9 @@
 import './stylesheets/style.css';
 import './stylesheets/mystyles.css';
 
+// Inicializando Svrip de Materialize
+document.addEventListener('DOMContentLoaded', () => {
+    const sideNav = document.querySelectorAll('.sidenav');
+    // eslint-disable-next-line no-undef
+    M.Sidenav.init(sideNav);
+});
