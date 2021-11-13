9ff66663b7eeca8f42f73f2954a196c3e6f41585
ERICKREYESG <rickreyes340@gmail.com>
2021-11-12 23:30:53 -0600

    •	SESION 41 MODELOS CON MONGOOSE

--- client/index.js
+++ client/index.js
@@ -2,9 +2,15 @@
 import './stylesheets/style.css';
 import './stylesheets/mystyles.css';
 
-// Inicializando Svrip de Materialize
+// Inicializando Script de materialize
 document.addEventListener('DOMContentLoaded', () => {
-    const sideNav = document.querySelectorAll('.sidenav');
+    // Inicializando todos los sidenav
+    document.querySelectorAll('.sidenav').forEach((sideNav) => {
     // eslint-disable-next-line no-undef
     M.Sidenav.init(sideNav);
+});
+// Inicializando el dropsown
+document.querySelectorAll('.dropdown-trigger')
+ // eslint-disable-next-line no-undef
+ .forEach((dropdown) => M.Dropdown.init(dropdown));
 });
